b36047548b739765536e81c47dabfe4cb0cb267b
sunnyblasters <sunnyblasters@ymail.com>
2024-03-21 23:50:03 +0500

    my first summary

---
+++ app.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myCalulator = await inquirer.prompt([
+    {
+        message: "Plz Enter First Number",
+        type: 'number',
+        name: 'firstNumber'
+    },
+    {
+        message: "Plz Enter Second Number",
+        type: 'number',
+        name: 'secondNumber'
+    },
+    {
+        message: "Plz Select the operator",
+        type: 'list',
+        name: 'operator',
+        choices: ['Addition', 'Multiply', 'Divide', 'Substraction']
+    }
+]);
+if (myCalulator.operator === 'Addition') {
+    console.log(myCalulator.firstNumber + myCalulator.secondNumber);
+}
+else if (myCalulator.operator === 'Divide') {
+    console.log(myCalulator.firstNumber / myCalulator.secondNumber);
+}
+else if (myCalulator.operator === 'Multiply') {
+    console.log(myCalulator.firstNumber * myCalulator.secondNumber);
+}
+else if (myCalulator.operator === 'Substraction') {
+    console.log(myCalulator.firstNumber - myCalulator.secondNumber);
+}
+else {
+    console.log('Something is wrong');
+}
